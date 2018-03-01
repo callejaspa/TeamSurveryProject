93bae7e05a3d5b2ea9cffb76287ddea27cb1eab5
Ben Blood <bluealikai@gmail.com>
2018-03-01 07:55:16 -0500

    add sign in

--- TeamSurveyProject/src/app/+signin/signin.component.ts
+++ TeamSurveyProject/src/app/+signin/signin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  constructor(public afAuth: AngularFireAuth, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  signInWithGoogle(): void {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(() => {
+      this.router.navigate(['/']);
+    });
   }
 
 }
