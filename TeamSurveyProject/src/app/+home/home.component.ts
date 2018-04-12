947c22b688459d40250f0ed301860595e69cf61e
Pauline Callejas <callejaspa@gmail.com>
2018-04-12 12:09:12 -0400

    Displayed team cards

--- TeamSurveyProject/src/app/+home/home.component.ts
+++ TeamSurveyProject/src/app/+home/home.component.ts
@@ -1,9 +1,12 @@
+//import { Teams } from './../models/teamsDummyData.model';
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestoreCollection, AngularFirestore } from 'angularfire2/firestore';
 import { DummyData, DummyDto } from '../models/dummyData.model';
 import { Subscription } from 'rxjs/Subscription';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
+import { AngularFireDatabase, FirebaseListObservable } from "angularfire2/database-deprecated";
+
 
 @Component({
   selector: 'app-home',
@@ -11,16 +14,20 @@ import * as firebase from 'firebase/app';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  //teamStream: FirebaseListObservable<Teams[]>
   //public dummyData: DummyData;
   //private dummyCollection: AngularFirestoreCollection<DummyDto>;
   //private authStateSubscription: Subscription;
 
-  constructor(private afs: AngularFirestore, private afAuth: AngularFireAuth) { 
+  constructor(private afs: AngularFirestore, private afAuth: AngularFireAuth) {
     //this.dummyCollection = afs.collection('items');
     //this.dummyData = new DummyData();
   }
 
   ngOnInit(): void {
+
+    //const firebasePath = ``
     // this.authStateSubscription = this.afAuth.authState.subscribe((user: firebase.User) => {
     //   if (user) {
     //     console.log(user.uid);
---
+++ TeamSurveyProject/src/app/models/teamsDummyData.model.ts
@@ -0,0 +1,24 @@
+// export class FirebaseFlatSnapshot {
+//     public $key?: string;
+
+//     constructor(obj? :any) {
+//         if(obj && obj.key) {
+//             this.$key = obj.$key;
+//         }
+//     }
+// }
+
+// export class Teams extends FirebaseFlatSnapshot {
+//     Course: string;
+//     Instructor: string;
+//     Name: string;
+//     Semester: string;
+
+//     constructor(obj?: any) {
+//         super(obj);
+//         this.Course = obj && obj.service || "";
+//         this.Instructor = obj && obj.service || "";
+//         this.Name= obj && obj.service || "";
+//         this.Semester = obj && obj.service || "";
+//     }
+// }
